a27d267943e13ee47f3b91d88af3ca7904169b31
Xichen(Sichem) Zhou <sichem.zh@gmail.com>
2026-07-09 05:38:48 -0400

    adding linux trippet for cstdint (needed for gcc-15 for materialx)
    Signed-off-by: Xichen(Sichem) Zhou <sichem.zh@gmail.com>

---
+++ triplets/x64-linux.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+
+# GCC 15 no longer transitively includes <cstdint> from other standard headers.
+# Many vcpkg ports (e.g. MaterialX) rely on this transitive include.
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -include stdint.h")
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -include cstdint")
