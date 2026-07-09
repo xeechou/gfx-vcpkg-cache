c05bfe18f108165a685f053729c61ebad430a58a
Xichen(Sichem) Zhou <sichem.zh@gmail.com>
2026-07-09 06:19:09 -0400

    fixing for enabling qsort_r for zstd
    Signed-off-by: Xichen(Sichem) Zhou <sichem.zh@gmail.com>

--- triplets/x64-linux.cmake
+++ triplets/x64-linux.cmake
@@ -5,5 +5,5 @@ set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
 # GCC 15 no longer transitively includes <cstdint> from other standard headers.
 # Many vcpkg ports (e.g. MaterialX) rely on this transitive include.
-set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -include stdint.h")
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS} -include stdint.h -D_GNU_SOURCE")
 set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -include cstdint")
